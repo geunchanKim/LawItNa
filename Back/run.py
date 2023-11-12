c25a6c9c0c5b59d3062ba3f44dac3a812b478916
JungRae Jo <wjdfoek3@knu.ac.kr>
2023-11-12 14:42:28 +0900

    local test

--- Back/run.py
+++ Back/run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 # text-bison을 사용하여 결과값을 반환하는 test.py 파일의 test Class 임포트
@@ -5,11 +6,11 @@ from test import test
 
 app = Flask(__name__)
 CORS(app)
-
 question_history = []
 
 @app.route('/', methods=['GET', 'POST'])
 def start():
+    chatbot = test()
     if request.method == 'GET':
         return jsonify({"question_history": question_history})
 
@@ -20,13 +21,13 @@ def start():
 
         print(question, " 수신함")
 
-        chatbot = test()
         answer = chatbot.service(question)
 
         print(answer, " 추출함")
-        
+
         response_data = {"question": question, "answer": answer}
         return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    # app.run(debug=False, host='0.0.0.0')    서버 연결할 때 사용
+    app.run(port = 5000)  #로컬에서 실험할 때 사용
